bf7f884a61999ea51d802ca3c5d760214b3de45b
Mihail Bohnearschi <Mihail.Bohnearschi@endava.com>
2017-03-15 13:31:07 +0200

    Added JDBC Configuration and HomeController

--- src/main/java/com/example/database/domain/Task.java
+++ src/main/java/com/example/database/domain/Task.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "task")
+@Table(name = "task_entity")
 @JsonIgnoreProperties("id")
 public class Task {
 
